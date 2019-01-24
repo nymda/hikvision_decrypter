ec5d7942836c72c4210e708b08c0aabc5128ee0d
jake hartley <puffleboy101@hotmail.co.uk>
2019-01-24 17:14:53 +0000

    --first commit

--- hikvisiondecrypter/Form1.Designer.cs
+++ hikvisiondecrypter/Form1.Designer.cs
@@ -28,13 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-2, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(438, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DROP FILE HERE\r\n";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(12, 67);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 3);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(12, 70);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(3, 350);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(12, 417);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(776, 3);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Black;
+            this.panel4.Location = new System.Drawing.Point(785, 69);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(3, 350);
+            this.panel4.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(442, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(206, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Remove non alphanumeric characters";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 429);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Hikvision Config Decrypter";
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- hikvisiondecrypter/Form1.cs
+++ hikvisiondecrypter/Form1.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +18,81 @@ namespace hikvisiondecrypter
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form_DragEnter(object sender, DragEventArgs e)
+        {
+            string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            byte[] key = { 0x73, 0x8B, 0x55, 0x44 };
+            byte[] fileContents = File.ReadAllBytes(FileList[0]);
+            byte[] xorOutput = new byte[fileContents.Length];
+
+            byte[] decryptedData = Decrypt(fileContents);
+
+            Console.WriteLine(FileList[0]);
+
+            for (int i = 0; i < decryptedData.Length; i++)
+            {
+                xorOutput[i] = (byte)(decryptedData[i] ^ key[i % key.Length]);
+            }
+
+            string output = Encoding.UTF8.GetString(xorOutput);
+            string outputrg;
+
+            if (checkBox1.Checked)
+            {
+                Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+                outputrg = rgx.Replace(output, "");
+            }
+            else
+            {
+                outputrg = output;
+            }
+
+            using (SaveFileDialog dlg = new SaveFileDialog())
+            {
+                dlg.Title = "Open Output File";
+                dlg.Filter = "Text Files | *.txt";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    string SafeFileName = Path.GetFileName(dlg.FileName);
+                    File.WriteAllText(dlg.FileName, outputrg);
+                }
+            }
+        }
+
+        public static byte[] Decrypt(byte[] cipherText)
+        {
+            byte[] cipherBytes = cipherText;
+            using (Aes encryptor = Aes.Create())
+            {
+                encryptor.Mode = CipherMode.ECB;
+                encryptor.Padding = PaddingMode.Zeros;
+                encryptor.Key = FromHex("27-99-77-f6-2f-6c-fd-2d-91-cd-75-b8-89-ce-0c-9a");
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = ms.ToArray();
+                }
+            }
+            return cipherText;
+        }
+
+        public static byte[] FromHex(string hex)
+        {
+            hex = hex.Replace("-", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
         }
     }
 }
